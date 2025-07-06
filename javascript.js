0f603e45eabb0fc135a67726f1a6f2f7c9ed50fc
Amar <amarkhadkabardiya1234@gmail.com>
2025-07-07 00:48:06 +0545

    Updated Javacript code

--- javascript.js
+++ javascript.js
@@ -10,6 +10,50 @@
 //         event.currentTarget.classList.add("active-link");
 //         document.getElementById(tabname).classList.add("active-tab");
 //     }
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxEwtJISaz9I9Pw2843SUCIl0XBK_vSeXfGPdpnHQY3NY-Id6vZMRnDBqnOBS3Ujy6C/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+//         form.addEventListener('submit', e => {
+//             msg.innerHTML = "Message sent successfully"; // Show message right away
+
+// fetch("YOUR_ENDPOINT_URL", {
+//     method: "POST",
+//     body: new FormData(form)
+// })
+// .then(response => {
+//     setTimeout(function () {
+//         msg.innerHTML = "";
+//     }, 2000); // Message disappears after 2 seconds
+//     form.reset();
+// })
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent page refresh
+
+    msg.innerHTML = "Message sent successfully"; // Show message immediately
+
+    fetch("YOUR_ENDPOINT_URL", {
+        method: "POST",
+        body: new FormData(form)
+    })
+        .then(response => {
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 2000); // 2 seconds
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+// e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//         .then(response => {
+//             msg.innerHTML = "Message sent succesfully"
+//             setTimeout(function () {
+//                 msg.innerHTML = ""
+//             }, 2000)
+//             form.reset()
+//         })
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -26,10 +70,10 @@ function opentab(tabname, event) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-    var sidemenu = document.getElementById("sidemenu");
-    function openmenu(){
-        sidemenu.style.right = "0";
-    }
-      function closemenu(){
-        sidemenu.style.right = "-200px";
-    }
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
