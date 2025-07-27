47cc8cf1453575ee6fbd97ef1fa11fe59a6ee909
Amar <amarkhadkabardiya1234@gmail.com>
2025-07-27 15:03:51 +0545

    Update: added new landing animation and styles

--- javascript.js
+++ javascript.js
@@ -1,32 +1,10 @@
-//    var tablinks = document.getElementsByClassName("tab-links");
-//     var tabcontents = document.getElementsByClassName("tab-contents");
-//     function opentab(tabname){
-//         for(tablink of tablinks){
-//             tablink.classList.remove("active-link");
-//         }
-//            for(tabcontent of tabcontens){
-//             tabcontent.classList.remove("active-tab");
-//         }
-//         event.currentTarget.classList.add("active-link");
-//         document.getElementById(tabname).classList.add("active-tab");
+
 //     }
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxEwtJISaz9I9Pw2843SUCIl0XBK_vSeXfGPdpnHQY3NY-Id6vZMRnDBqnOBS3Ujy6C/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
 
-//         form.addEventListener('submit', e => {
-//             msg.innerHTML = "Message sent successfully"; // Show message right away
 
-// fetch("YOUR_ENDPOINT_URL", {
-//     method: "POST",
-//     body: new FormData(form)
-// })
-// .then(response => {
-//     setTimeout(function () {
-//         msg.innerHTML = "";
-//     }, 2000); // Message disappears after 2 seconds
-//     form.reset();
-// })
 form.addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent page refresh
 
@@ -44,15 +22,7 @@ form.addEventListener("submit", function (e) {
         })
         .catch(error => console.error('Error!', error.message))
 })
-// e.preventDefault()
-//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-//         .then(response => {
-//             msg.innerHTML = "Message sent succesfully"
-//             setTimeout(function () {
-//                 msg.innerHTML = ""
-//             }, 2000)
-//             form.reset()
-//         })
+
 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
@@ -77,3 +47,32 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px";
 }
+
+
+
+// Infinite smooth word animation
+const animatedHeading = document.getElementById("animated-heading");
+const heroSentence = "Hi, I'm Amar Khadka From Nepal";
+const wordList = heroSentence.split(" ");
+
+function runWordAnimation() {
+  animatedHeading.innerHTML = "";
+
+  wordList.forEach((word, i) => {
+    const wordSpan = document.createElement("span");
+    wordSpan.textContent = word;
+    animatedHeading.appendChild(wordSpan);
+
+    setTimeout(() => {
+      wordSpan.classList.add("visible");
+    }, i * 400);
+  });
+
+  // Restart after sentence completes
+  setTimeout(() => {
+    runWordAnimation();
+  }, wordList.length * 400 + 1500);
+}
+
+runWordAnimation();
+
